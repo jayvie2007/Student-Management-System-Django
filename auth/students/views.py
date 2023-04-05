557f08736f244de946b38d6fc35e593fa67de711
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-05 14:17:35 +0800

    Added Modals which display info

--- auth/students/views.py
+++ auth/students/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from rest_framework.decorators import APIView
 from .models import Student
 # Create your views here.
@@ -8,4 +10,10 @@ def index(request):
     return render(request, 'students/index.html',{
         'students': Student.objects.all()
         })
+#the variable 'students' is used to call all the data inside the Student 
+#model and hence to be called by loop inside the templates
+
+def view_student(request, id):
+    students = students.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
 
