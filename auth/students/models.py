b745852a76e1b2b169ffcb4b811e9ac9fd110450
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-05 09:36:09 +0800

    Added student models

--- auth/students/models.py
+++ auth/students/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_number = models.PositiveBigIntegerField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    course = models.CharField(max_length=50)
+    gpa = models.FloatField()
+
+    def __str__(self):
+        return f'Student: {self.last_name} {self.first_name}'
