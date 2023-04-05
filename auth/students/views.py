e528da0dcec3c0e36db36ed8416a3f4332edd749
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-05 10:17:52 +0800

    Added Bootstrap

--- auth/students/views.py
+++ auth/students/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework.decorators import APIView
+from .models import Student
 # Create your views here.
 
 #@APIView(['GET', 'POST'])
 def index(request):
-    return render(request, 'students/index.html')
+    return render(request, 'students/index.html',{
+        'students': Student.objects.all()
+        })
+
